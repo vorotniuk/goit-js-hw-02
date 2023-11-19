e150c3fe17a7fcc30c8448eb886a235304c11bae
Yaroslav Vorotniuk <slavikvorotnuk@gmail.com>
2023-11-19 17:36:40 +0200

    push

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function checkForSpam(message) {
+    const lowercasedMessage = message.toLowerCase();
+    return lowercasedMessage.includes("spam") || lowercasedMessage.includes("sale");
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
