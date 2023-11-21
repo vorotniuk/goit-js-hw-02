3010d8bbd70e49dd2c6015d2a40e99ff66e6b845
Yaroslav Vorotniuk <slavikvorotnuk@gmail.com>
2023-11-21 18:54:35 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,27 @@
 function getShippingCost(country) {
     let message;
-    switch (country.toLowerCase()) {
+    switch (country) {
         case "china":
             message = `Shipping to ${country} will cost 100 credits`;
             break;
-            case "chile":
-                message = `Shipping to ${country} will cost 250 credits`;
-                break;
-                case "australia":
-                    message = `Shipping to ${country} will cost 170 credits`;
-                    break;
-                    case "jamaica":
-                        message = `Shipping to ${country} will cost 100 credits`;
-                        break;
-                        default:
-                            message = "Sorry, there is no delivery to your country";
-                        }
-                        return message;
-                    }
-                    
-                    
-                    console.log(getShippingCost("Australia"));
-                    console.log(getShippingCost("Germany"));
-                    console.log(getShippingCost("China"));
-                    console.log(getShippingCost("Chile"));
-                    console.log(getShippingCost("Jamaica"));
-                    console.log(getShippingCost("Sweden"));
+        case "chile":
+            message = `Shipping to ${country} will cost 250 credits`;
+            break;
+        case "australia":
+            message = `Shipping to ${country} will cost 170 credits`;
+            break;
+        case "jamaica":
+            message = `Shipping to ${country} will cost 120 credits`;
+            break;
+        default:
+            message = "Sorry, there is no delivery to your country";
+        }
+        return message;
+}
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
